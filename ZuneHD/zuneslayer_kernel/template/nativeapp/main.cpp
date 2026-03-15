60ebf10c31ad207fc89d280bd0c1864c439af96b
Dávid Balatoni <balcsida@gmail.com>
2026-03-15 13:38:31 +0100

    fix(payload): remove all kwasm/wasm files from project, fix SuppressReboot conflict

--- ZuneHD/zuneslayer_kernel/template/nativeapp/main.cpp
+++ ZuneHD/zuneslayer_kernel/template/nativeapp/main.cpp
@@ -13,11 +13,8 @@
 #include "xutility.h"
 
 // zdksystem.h pulls in compclient.h -> EGL/egl.h, declare what we need directly
-extern "C" {
-	void ZDKSystem_ShowMessageBox(LPCWSTR text, int type);
-	#define MESSAGEBOX_TYPE_OK 0
-	void SuppressReboot();
-}
+extern "C" void ZDKSystem_ShowMessageBox(LPCWSTR text, int type);
+#define MESSAGEBOX_TYPE_OK 0
 
 // VirtualCopy not in OpenZDK headers, declare from WinCE API
 extern "C" BOOL VirtualCopy(LPVOID lpvDest, LPVOID lpvSrc, DWORD cbSize, DWORD fdwProtect);
